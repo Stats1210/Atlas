a8f7e4837cb10c3d7d97af8eb0b9531efd66b76c
Rudi Van Vuuren <rudi.vuuren@heygaz.com>
2026-05-09 23:58:26 +0200

    Fix base path capitalisation

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/atlas' : ''
+			base: process.env.NODE_ENV === 'production' ? '/Atlas' : ''
 		}
 	}
 };
